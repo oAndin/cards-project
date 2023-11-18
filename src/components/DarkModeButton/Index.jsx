a8e3ebd50a457acdccd520cfa11977f3a6192ff3
oAndin <00andherson@gmail.com>
2023-11-18 07:14:38 -0300

    NOT-WORKING_ WORK-PC&CSS_updated

--- src/components/DarkModeButton/Index.jsx
+++ src/components/DarkModeButton/Index.jsx
@@ -3,19 +3,19 @@ import { ThemeContext } from "../../context/ThemeContext/Index"
 
 const DarkModeButton = () => {
 
-  const { toggleTheme, darkMode } = useContext(ThemeContext);
+  const { toggleTheme, bgColor, textColor } = useContext(ThemeContext);
 
 
   return (
     <>
       <div onClick={() => {
         toggleTheme
-        console.log(darkMode);
+        console.log(textColor, bgColor);
       }}
-        className="border-2 border-black p-1 rounded-full cursor-pointer flex">
-        <span className="w-11 h-5 p-1 flex rounded-full bg-black items-center">
+        className={`border-2 border-${textColor} p-1 rounded-full cursor-pointer flex`}>
+        <span className={`w-11 h-5 p-1 flex rounded-full bg-${bgColor} items-center`}>
           <span
-            className="bg-white rounded-full w-3 h-3"></span>
+            className={`bg-${textColor} rounded-full w-3 h-3`}></span>
         </span>
       </div>
     </>
