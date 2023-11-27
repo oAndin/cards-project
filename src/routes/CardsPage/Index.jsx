98e6d9737a005d24d4acd7c795f9cc02bcbd76db
oAndin <00andherson@gmail.com>
2023-11-26 23:10:46 -0300

    working basic carousel

--- src/routes/CardsPage/Index.jsx
+++ src/routes/CardsPage/Index.jsx
@@ -3,7 +3,6 @@ import { MdArrowBackIos } from "react-icons/md";
 import { useEffect } from "react";
 import { useState } from "react";
 import API from "../../services";
-import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css/effect-cards';
 
 const CardsPage = () => {
@@ -22,7 +21,7 @@ const CardsPage = () => {
 
   const carouselCards = cards.map(card => (
     <div key={card.id}
-      className="border-2 border-black w-1/4 h-1/2 p-5 m-5">
+      className="border-2 border-black min-w-1/2 w-1/4 max-w-full h-1/2 p-5 m-5">
       <h1>{card.title}</h1>
       <h3>{card.question}</h3>
       <h5>{card.answer}</h5>
@@ -33,11 +32,19 @@ const CardsPage = () => {
     getCards();
   }, []);
 
-  console.log(cards);
+  const [position, setPosition] = useState(0);
+
   return (
     <>
       <div className="h-screen p-10 flex items-center justify-center">
-        {/* <MdArrowBackIos className="text-5xl hover:scale-125 duration-300 items-center" />
+        <div className="flex items-center justify-center">
+          <MdArrowBackIos className="text-5xl hover:scale-125 duration-300 items-center"
+            onClick={() => setPosition(position === 0 ? 0 : position - 1)} />
+          {carouselCards[position]}
+          <MdArrowBackIos className="rotate-180 text-5xl hover:scale-125 duration-300 items-center"
+            onClick={() => setPosition(position + 1 === carouselCards.length ? position : position + 1)} />
+        </div>
+        {/* 
         {cards ? (
           cards.map(card => (
             <div key={card.id}
@@ -48,7 +55,7 @@ const CardsPage = () => {
             </div>
           ))
         ) : <h1>Create a new card</h1>}
-        <MdArrowBackIos className="rotate-180 text-5xl hover:scale-125 duration-300 items-center" /> */}
+         */}
         {/* <Swiper
           effect={'cards'}
           grabCursor={true}>
