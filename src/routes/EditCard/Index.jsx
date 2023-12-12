43088bcec9ec92d0111f73cd8d888c0e62fac20a
oAndin <00andherson@gmail.com>
2023-12-11 21:35:10 -0300

    updated code

--- src/routes/EditCard/Index.jsx
+++ src/routes/EditCard/Index.jsx
@@ -1,7 +1,18 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import API from "../../services";
 
 const EditCard = () => {
   const { id } = useParams();
+  const [card, setCard] = useState()
+  useEffect((id) => {
+    API.get(`/cards/${id}`)
+    const response = response.data;
+    setCard(response)
+  }, [])
+
+  console.log(card);
+
   return (
     <>
       <h1>
