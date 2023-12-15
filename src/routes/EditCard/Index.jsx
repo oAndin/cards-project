0669949726f3d31556bbf2312ccc82a3b7fb63f5
oAndin <00andherson@gmail.com>
2023-12-14 21:30:03 -0300

    axios get working, thanks gleidson

--- src/routes/EditCard/Index.jsx
+++ src/routes/EditCard/Index.jsx
@@ -4,17 +4,21 @@ import API from "../../services";
 
 const EditCard = () => {
   const { id } = useParams();
-  const [card, setCard] = useState()
-  const [title, setTitle] = useState();
+  const [card, setCard] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  // const [title, setTitle] = useState(); // Case a variable is needed 
 
   const getCard = async (id) => {
     try {
       const response = await API.get(`cards/${id}`);
       const data = response.data;
-      setCard(data)
-        ;
+      setCard(data);
     } catch (error) {
       console.log(error);
+    }
+    finally {
+      setIsLoading(false);
     }
   }
 
@@ -27,10 +31,10 @@ const EditCard = () => {
   return (
     <>
       <h1>
-        {{ card } &&
+        {!isLoading &&
           <div>
             Edit Card: {id}
-            {title}
+            {card.title}
             {/* {card.title ?
               <h1>Title: { }</h1>
               :
